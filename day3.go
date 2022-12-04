32adf867c41f9b9eb927e9dcec23fd3a4548348f
fs098 <filipesantos@tutanota.com>
2022-12-04 17:26:31 +0000

    cleaner code

--- day3.go
+++ day3.go
@@ -3,71 +3,72 @@ package main
 import "fmt"
 
 func day3(filename string) {
-	var (
-		data  = getLines(filename)
-		part1 = sumOfPriorities1(data)
-		part2 = sumOfPriorities2(data)
-	)
+	data := getLines(filename)
+	part1, part2 := sumOfPriorities(data)
 
 	fmt.Println("Day 3")
 	fmt.Println("Part 1:", part1)
 	fmt.Println("Part 2:", part2)
 }
 
-func sumOfPriorities1(data []string) int {
-	result := 0
-
-	for _, val := range data {
-		var (
-			middle     = len(val) / 2
-			partition1 = val[:middle]
-			partition2 = val[middle:]
-
-			set = make(map[byte]bool)
-		)
-
-		for i := 0; i < len(partition1); i++ {
-			set[partition1[i]] = true
-		}
-
-		for i := 0; i < len(partition2); i++ {
-			if set[partition2[i]] {
-				result += priority(partition2[i])
-				break
-			}
-		}
-	}
-	return result
-}
-
-func sumOfPriorities2(data []string) int {
-	result := 0
+func sumOfPriorities(data []string) (int, int) {
+	part1 := 0
+	part2 := 0
 
 	for i := 0; i < len(data); i += 3 {
 		groupData := []string{data[i], data[i+1], data[i+2]}
 		foundSet := make(map[byte]int) // foundSet[x] must == 3
 
-		for _, val := range groupData {
-			alreadyFound := make(map[byte]bool)
-
-			for j := 0; j < len(val); j++ {
-				item := val[j]
-
-				if alreadyFound[item] {
-					continue
-				}
-				alreadyFound[item] = true
-				foundSet[item]++
-			}
+		for _, line := range groupData {
+			part1 += priorityFromLine(line)
+			foundSetFromLine(line, &foundSet)
 		}
 
 		for key, val := range foundSet {
 			if val == 3 {
-				result += priority(key)
+				part2 += priority(key)
 			}
 		}
 	}
-	return result
+	return part1, part2
+}
+
+// part 1
+func priorityFromLine(line string) int {
+	var (
+		middle     = len(line) / 2
+		partition1 = line[:middle]
+		partition2 = line[middle:]
+
+		set = make(map[byte]bool)
+	)
+
+	for i := 0; i < len(partition1); i++ {
+		set[partition1[i]] = true
+	}
+
+	for i := 0; i < len(partition2); i++ {
+		if set[partition2[i]] {
+			return priority(partition2[i])
+		}
+	}
+	return 0
+}
+
+// part 2
+func foundSetFromLine(line string, set *map[byte]int) {
+	foundSet := *set
+	alreadyFound := make(map[byte]bool)
+
+	for j := 0; j < len(line); j++ {
+		item := line[j]
+
+		if alreadyFound[item] {
+			continue
+		}
+		alreadyFound[item] = true
+		foundSet[item]++
+	}
 }
 
 func priority(b byte) int {
